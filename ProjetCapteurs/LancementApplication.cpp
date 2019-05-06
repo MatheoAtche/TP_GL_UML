f725cfb30f46302ad8613e0e6ff148ca73c3feab
SophieLIF <laboucheix.sophie@gmail.com>
2019-05-06 17:25:25 +0200

    Bon main

--- ProjetCapteurs/LancementApplication.cpp
+++ ProjetCapteurs/LancementApplication.cpp
@@ -1,3 +1,5 @@
+#define CRT_SECURE_NO_WARNINGS
+
 using namespace std;
 #include <iostream>
 
@@ -7,9 +9,14 @@ using namespace std;
 #include "Mesure.h"
 #include "OperationsDonnees.h"
 
-int main(int nbArg, string args[]) {
+int main(int argc, char * argv[]) {
 
+	//Obligé pour qu'on voit qql chose s'affiche !
+	char * a = new char[10];
 	cout << "coucou" << endl;
+	cin >> a;
+	cout << a;
+	return 0;
 
 
 }
