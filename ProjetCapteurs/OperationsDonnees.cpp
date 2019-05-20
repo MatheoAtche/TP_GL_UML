f0fbcbd234f750bd4476f40be8e0a05dc87c959a
Hermora <matheo.atche@gmail.com>
2019-05-20 16:59:47 +0200

    ajout de commentaires

--- ProjetCapteurs/OperationsDonnees.cpp
+++ ProjetCapteurs/OperationsDonnees.cpp
@@ -1,15 +1,15 @@
 /***********************************************************************************************************************************
 						   OperationsDonnees  -  description
 							 -------------------
-	d�but                : 06/05/2019
-	copyright            : (C) 2019 par Alice d'Oncieu, Andrea Croc, Sophie Laboucheix, Math�o Atche
+	debut                : 06/05/2019
+	copyright            : (C) 2019 par Alice d'Oncieu, Andrea Croc, Sophie Laboucheix, Matheo Atche
 *************************************************************************/
 
-//---------- R�alisation de la classe <OperationsDonnees> (fichier OperationsDonnees.cpp) --
+//---------- Realisation de la classe <OperationsDonnees> (fichier OperationsDonnees.cpp) --
 
 //---------------------------------------------------------------- INCLUDE
 
-//-------------------------------------------------------- Include syst�me
+//-------------------------------------------------------- Include systeme
 using namespace std;
 #include <iostream>
 
@@ -20,17 +20,20 @@ using namespace std;
 #define TAILLE 20
 //---------------------------------------------------- Variables de classe
 
-//----------------------------------------------------------- Types priv�s
+//----------------------------------------------------------- Types prives
 
 
 //----------------------------------------------------------------- PUBLIC
 //-------------------------------------------------------- Fonctions amies
 
-//----------------------------------------------------- M�thodes publiques
+//----------------------------------------------------- Methodes publiques
 
 
 int OperationsDonnees::qualiteAirMoyenne(string dateDebut, string dateFin, double latitude1, double longitude1, double latitude2, double longitude2, ComposantAir* o3, ComposantAir* no2, ComposantAir* so2, ComposantAir* pm10, map<string, Capteur>* tabCapteurs)
-//
+// Algorithme :
+// Les moyennes de chaque particule sont calculees a l'aide des methodes des ComposantsAir
+// elles sont ensuite comparées aux valeurs permettant de determiner l'indice ATMO,
+// une fois l'indice trouve, il est retourne
 {
 	double moyO3 = o3->moyenne(dateDebut,dateFin,latitude1,longitude1,latitude2,longitude2, tabCapteurs);
 	double moyNO2 = no2->moyenne(dateDebut,dateFin,latitude1,longitude1,latitude2,longitude2, tabCapteurs);
@@ -74,18 +77,13 @@ int OperationsDonnees::qualiteAirMoyenne(string dateDebut, string dateFin, doubl
 		indice =10;
 	}
 	return indice;
-} //----- Fin de M�thode
-
-/*set<Capteur> OperationsDonnees::bonFonctionnementCapteurs(string dateDebut, string dateFin, ComposantAir* o3, ComposantAir* no2, ComposantAir* so2, ComposantAir* pm10)
-// Algorithme :
-//
-{
-} //----- Fin de M�thode
-*/
+} //----- Fin de qualiteAirMoyenne
 
 int OperationsDonnees::qualiteAirPointFixe(string dateDebut, string dateFin, double latitude1, double longitude1, ComposantAir* o3, ComposantAir* no2, ComposantAir* so2, ComposantAir* pm10, map<string, Capteur>* tabCapteurs)
 // Algorithme :
-//
+// L'algo est presque le meme que pour qualiteAirMoyenne, seulement nous entrons deux fois
+// les memes coordonnees pour la localisation dans le calcul de moyenne, de sorte à ne calculer
+// la qualite de l'air que sur un capteur
 {
 	double moyO3 = o3->moyenne(dateDebut,dateFin,latitude1,longitude1,latitude1,longitude1, tabCapteurs);
 	double moyNO2 = no2->moyenne(dateDebut,dateFin,latitude1,longitude1,latitude1,longitude1, tabCapteurs);
@@ -130,9 +128,13 @@ int OperationsDonnees::qualiteAirPointFixe(string dateDebut, string dateFin, dou
 	}
 	return indice;
 	return NULL;
-} //----- Fin de M�thode
+} //----- Fin de qualitéAirPointFixe
 
 set<string> OperationsDonnees::bonFonctionnementCapteurs(string dateDebut, string dateFin, ComposantAir* o3, ComposantAir* no2, ComposantAir* so2, ComposantAir* pm10)
+// Algorithme :
+// La totalite des mesures capteurs sont recuperees selon le type de particule, puis pour chaque capteur
+// les mesures sont parcourues sur la periode donnee, si plus de 10% d'entre elles sont nulles,
+// le capteur est considere comme defaillant et son identifiant est stocke dans la structure de retour
 {
 	set<string> CapteurNonFonctionnel;
 	tabMesure_type mesO3 = o3->getTabMesure();
@@ -216,31 +218,9 @@ set<string> OperationsDonnees::bonFonctionnementCapteurs(string dateDebut, strin
 		}
 	}
 	return CapteurNonFonctionnel;
-}
-
-//------------------------------------------------- Surcharge d'op�rateurs
-/*OperationsDonnees & OperationsDonnees::operator = (const OperationsDonnees &unOperationsDonnees)
-// Algorithme :
-//
-{
-} //----- Fin de operator =
-*/
-
-//-------------------------------------------- Constructeurs - destructeur
-/*OperationsDonnees::OperationsDonnees(const OperationsDonnees &unOperationsDonnees)
-// Algorithme :
-//
-{
-#ifdef MAP
-	cout << "Appel au constructeur de copie de <OperationsDonnees>" << endl;
-#endif
-} //----- Fin de OperationsDonnees (constructeur de copie)
-*/
-
+} //----- Fin de bonFonctionnementCapteurs
 
 OperationsDonnees::OperationsDonnees()
-// Algorithme :
-//
 {
 #ifdef MAP
 	cout << "Appel au constructeur de <OperationsDonnees>" << endl;
@@ -249,17 +229,8 @@ OperationsDonnees::OperationsDonnees()
 
 
 OperationsDonnees::~OperationsDonnees()
-// Algorithme :
-//
 {
 #ifdef MAP
 	cout << "Appel au destructeur de <OperationsDonnees>" << endl;
 #endif
 } //----- Fin de ~OperationsDonnees
-
-
-//------------------------------------------------------------------ PRIVE
-
-//----------------------------------------------------- M�thodes prot�g�es
-
-//------------------------------------------------------- M�thodes priv�es
